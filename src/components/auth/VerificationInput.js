637a0129ddae71cc9e96fa307f4ae3d8443a18e7
junglimit <jaehan_jjh@naver.com>
2024-07-12 11:31:14 +0900

    패스워드 받기 , 후처리

--- src/components/auth/VerificationInput.js
+++ src/components/auth/VerificationInput.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './SignUpForm.module.scss';
-import { debounce } from "lodash";
-import { AUTH_URL } from "../../config/host-config";
+import { debounce } from 'lodash';
+import { AUTH_URL } from '../../config/host-config';
 
-const VerificationInput = ({email, onSuccess}) => {
+const VerificationInput = ({ email, onSuccess }) => {
 
     // 여러개의 컴포넌트에 ref를 거는 방법
     const inputsRef = useRef([]);
@@ -17,42 +17,48 @@ const VerificationInput = ({email, onSuccess}) => {
     // 타이머 시간
     const [timer, setTimer] = useState(300);
 
+
     // 다음 칸으로 포커스를 이동하는 함수
     const focusNextInput = (index) => {
-        if(index < inputsRef.current.length) {
+
+        if (index < inputsRef.current.length) {
             inputsRef.current[index].focus();
         }
     };
 
-    // 서버에 검증 요청 보내기
+    // 서버에 검증요청 보내기
     const verifyCode = debounce(async (code) => {
-        console.log('요청 전송: ', code);
+        // console.log('요청 전송!: ', code);
 
         const response = await fetch(`${AUTH_URL}/code?email=${email}&code=${code}`);
         const flag = await response.json();
 
         // console.log('코드검증: ', flag);
         // 검증에 실패했을 때
-        if(!flag) {
-            setError('유효하지 않거나 만료된 코드입니다. 인증코드를 재발송 합니다.');
+        if (!flag) {
+            setError('유효하지 않거나 만료된 코드입니다. 인증코드를 재발송합니다.');
             // 기존 인증코드 상태값 비우기
             setCodes(Array(4).fill(''));
+            // 타이머 리셋
             setTimer(300);
 
             inputsRef.current[0].focus();
             return;
         }
 
+        // 검증 성공 시
+        onSuccess();
+        setError('');
+
     }, 1500);
 
     const changeHandler = (index, inputValue) => {
 
-
-        const updatedCodes = ([... codes]);
+        const updatedCodes = [ ...codes ];
         updatedCodes[index - 1] = inputValue;
         console.log(updatedCodes);
 
-        // codes 변수에 입력한 숫자 담아놓기
+        // codes변수에 입력한 숫자 담아놓기
         setCodes(updatedCodes);
 
         // 입력이 끝나면 다음 칸으로 포커스 이동
@@ -60,17 +66,14 @@ const VerificationInput = ({email, onSuccess}) => {
 
         // 입력한 숫자 합치기
         // join() : 배열안에 있는 요소를 전부 연결
-        if(updatedCodes.length === 4 && index === 4){
-            const code = updatedCodes.join(''); // 구분자 비워두기
-
-            // 서버로 인증코드 검증 요청
+        if (updatedCodes.length === 4 && index === 4) {
+            const code = updatedCodes.join('');
+            // 서버로 인증코드 검증 요청 전송
             verifyCode(code);
-            onSuccess();
         }
 
-
-
     };
+
 
     useEffect(() => {
         // 처음엔 첫번째 칸에 포커싱
@@ -78,12 +81,12 @@ const VerificationInput = ({email, onSuccess}) => {
 
         // 타이머 설정
         const intervalId = setInterval(() => {
-            setTimer(prevTime => prevTime > 0 ? prevTime - 1 : 0)
-        },1000)
+            setTimer(prevTime => prevTime > 0 ? prevTime - 1 : 0);
+        }, 1000);
 
         // 타이머 리셋
         return () => {
-            clearInterval(intervalId)
+            clearInterval(intervalId);
         };
 
     }, []);
@@ -93,9 +96,9 @@ const VerificationInput = ({email, onSuccess}) => {
             <p>Step 2: 이메일로 전송된 인증번호 4자리를 입력해주세요.</p>
             <div className={styles.codeInputContainer}>
                 {
-                    Array.from(new Array(4)).map((_, index)=> (
+                    Array.from(new Array(4)).map((_, index) => (
                         <input
-                            ref={($input) => inputsRef.current[index] = $input }
+                            ref={($input) => inputsRef.current[index] = $input}
                             key={index}
                             type='text'
                             className={styles.codeInput}
@@ -105,10 +108,11 @@ const VerificationInput = ({email, onSuccess}) => {
                         />
                     ))
                 }
-
             </div>
-            <div className={styles.timer}>{`${'0' + Math.floor(timer / 60)}:${('0' + (timer % 60)).slice(-2)}`}</div>
-            {error && <p className={styles.errorMessage}>{error}</p>}
+            <div className={styles.timer}>
+                {`${'0' + Math.floor(timer / 60)}:${('0' + (timer % 60)).slice(-2)}`}
+            </div>
+            { error && <p className={styles.errorMessage}>{error}</p> }
         </>
     );
 };
