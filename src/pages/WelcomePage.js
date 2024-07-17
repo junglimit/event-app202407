f302a45aa1b0ba9dd1c26b83219da4d1bd69f2db
junglimit <jaehan_jjh@naver.com>
2024-07-17 10:20:26 +0900

    로그인 조건부 렌더링 사용해보기

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import LoginForm from "../components/auth/LoginForm";
+import React, { useEffect, useState } from 'react';
+import LoginForm from '../components/auth/LoginForm';
+import Main from '../components/Main';
 
 const WelcomePage = () => {
-    return <LoginForm />
+
+    console.log('WelcomePage 실행!');
+
+    // 로그인 여부
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const userData = localStorage.getItem('userData');
+
+        if (userData) setIsLoggedIn(true);
+        else setIsLoggedIn(false);
+
+    }, [isLoggedIn]);
+
+    return (
+        <>
+            { <LoginForm />}
+            {<Main />}
+        </>
+    );
 };
 
 export default WelcomePage;
