f7ea67c33927786b148ed82e6788615473e71fc6
junglimit <jaehan_jjh@naver.com>
2024-07-20 13:18:44 +0900

    등급업 처리

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import {Form, useRouteLoaderData} from "react-router-dom";
+import { Form, useNavigate, useRouteLoaderData } from 'react-router-dom';
+import { AUTH_URL } from '../config/host-config';
 
 const Main = () => {
 
-    const {role, email} = useRouteLoaderData(('user-data'))
+    const navigate = useNavigate();
+
+    const { role, email, token } = useRouteLoaderData('user-data');
+
+    const promoteHandler = async (e) => {
+
+        const response = await fetch(`${AUTH_URL}/promote`, {
+            method: 'PUT',
+            headers: { 'Authorization': 'Bearer ' + token }
+        });
+
+        if (!response.ok) {
+            alert('올바르지 않은 요청입니다.');
+            return;
+        }
+
+        const responseData = await response.json(); // 새 토큰 받기
+
+        // 토큰 갱신
+        localStorage.setItem('userData', JSON.stringify(responseData));
+
+        alert('프리미엄회원이 되신 것을 축하합니다!');
+        navigate('/');
+
+    };
 
     return (
         <>
             <h2>{email}님 환영합니다.</h2>
             <h3>현재 권한: [ {role} ]</h3>
-            {/* 다른 라우트의 액션을 트리거 하는 방법 (path 가 logout 인 라우트의 액션을 발동시킴) */}
+
+            {
+                role === 'COMMON' &&
+                <button
+                    style={{
+                        background: 'orangered',
+                        color: 'white'
+                    }}
+                    onClick={promoteHandler}
+                >
+                    Go To Premium
+                </button>
+            }
+
+            {/* 다른 라우트의 액션을 트리거하는 방법 */}
             <Form action='/logout' method='POST'>
                 <button>Logout</button>
             </Form>
