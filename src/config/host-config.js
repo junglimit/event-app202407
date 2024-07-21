43a1b3215270e0fe4397ef4e6f6436621e9f2cbd
junglimit <jaehan_jjh@naver.com>
2024-07-21 22:26:49 +0900

    등급업 처리

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -8,6 +8,7 @@ const clientHostName = window.location.hostname;
 
 let backendHostName;
 
+
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:' + LOCAL_PORT;
 } else if (clientHostName === 'www.bananagrape.co.kr') {
